8d262451e6486f68a03df6a9203a1969833c7b77
Dorweiler <louis.dorweiler@dlr.de>
2023-10-31 15:08:46 +0100

    Add build ps script for markdown to pdf builds.

---
+++ build.ps1
@@ -0,0 +1,12 @@
+param([string]$file_name)
+
+if ([string]::IsNullOrEmpty($file_name)) {
+    Write-Host "Error: No file name provided. Please provide a file name."
+    exit 1
+}
+
+$file_base_name = [System.IO.Path]::GetFileNameWithoutExtension($file_name)
+pandoc.exe $file_name -o .\$file_base_name.pdf --pdf-engine tectonic --filter pandoc-plantuml -V colorlinks=true -V urlcolor="purple" -V mainfont="Montserrat" -V monofont="JetBrainsMono"
+Start-Process .\$file_base_name.pdf
+Remove-Item .\plantuml-images\* -Force
+
