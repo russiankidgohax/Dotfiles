96f5302992787d539ad750a0af03c3c22aec6830
Dorweiler <louis.dorweiler@dlr.de>
2023-11-02 10:46:11 +0100

    Add -s (standalone) build to build.ps1.

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ if ([string]::IsNullOrEmpty($file_name)) {
 }
 
 $file_base_name = [System.IO.Path]::GetFileNameWithoutExtension($file_name)
-pandoc.exe $file_name -o .\$file_base_name.pdf --pdf-engine tectonic --filter pandoc-plantuml -V colorlinks=true -V urlcolor="purple" -V mainfont="Montserrat" -V monofont="JetBrainsMono"
+pandoc.exe $file_name -s -o .\$file_base_name.pdf --pdf-engine tectonic --filter pandoc-plantuml -V colorlinks=true -V urlcolor="purple" -V mainfont="Montserrat" -V monofont="JetBrainsMono"
 Start-Process .\$file_base_name.pdf
 Remove-Item .\plantuml-images\* -Force
 
