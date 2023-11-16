97b96e0727cced05d3397266c568ab5e3ce4d0e4
Dorweiler <louis.dorweiler@dlr.de>
2023-11-16 18:16:45 +0100

    Update profile.

--- WorkAutomation/Microsoft.PowerShell_profile.ps1
+++ WorkAutomation/Microsoft.PowerShell_profile.ps1
@@ -1,31 +1,44 @@
-Set-PSReadLineKeyHandler -Chord "Ctrl+w" -Function BackwardKillWord
 oh-my-posh init pwsh --config 'C:\Users\dorw_lu\Documents\own-docs\Dotfiles\themes\easy-owned.omp.json' | Invoke-Expression
+Set-PSReadLineKeyHandler -Chord "Ctrl+w" -Function BackwardKillWord
 
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
 # See https://ch0.co/tab-completion for details.
-
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
-Set-Alias "ex" "explorer"
-Set-Alias "which" "Get-Command"
+Set-Alias ex explorer
+Set-Alias which Get-Command
+Set-Alias lg lazygit
+Set-Alias lazy lazygit
+Set-Alias c conda
+
+function Conda-Deactivate {
+  conda deactivate
+}
+Set-Alias cda Conda-Deactivate
+
+function Conda-Activate {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$env
+    )
+    conda activate $env
+}
+Set-Alias ca Conda-Activate
+
 
 function Start-Activity-Browser {
   conda activate ab
   activity-browser
 }
-
 Set-Alias "ab" Start-Activity-Browser
 
 function Run-Cargo {
   cargo run $args
 }
 Set-Alias cr Run-Cargo
-Set-Alias lg lazygit
-Set-Alias lazy lazygit
-C:\\Users\\dorw_lu\\Documents\\own-docs\\Dotfiles\\WorkAutomation\\copy_own_docs_to_H_drive.ps1
+
+if ($env:WORK_PC = 1) {
+  C:\\Users\\dorw_lu\\Documents\\own-docs\\Dotfiles\\WorkAutomation\\copy_own_docs_to_H_drive.ps1
+}
