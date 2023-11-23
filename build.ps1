04c5c0fdb481ae820f24650af7a4df8df5217ecb
Louis Dorweiler <louis.dorweiler@gmx.de>
2023-11-23 20:59:23 +0100

    Fix further issues so that build.ps1 runs on MacOS (and possibly Linux) just fine.

--- build.ps1
+++ build.ps1
@@ -7,20 +7,20 @@ if ([string]::IsNullOrEmpty($file_name)) {
 
 $file_base_name = [System.IO.Path]::GetFileNameWithoutExtension($file_name)
 
-if (Test-Path ".\$file_base_name.pdf") {
-    Remove-Item ".\$file_base_name.pdf"
+if (Test-Path "$file_base_name.pdf") {
+    Remove-Item "$file_base_name.pdf"
 }
 
-pandoc "$file_name" -s -o ".\$file_base_name.pdf" --pdf-engine xelatex --filter pandoc-plantuml -V colorlinks=true -V urlcolor="purple" -V mainfont="Montserrat" -V monofont="JetBrainsMono-Bold"
+pandoc "$file_name" -s -o "$file_base_name.pdf" --pdf-engine xelatex --filter pandoc-plantuml -V colorlinks=true -V urlcolor="purple" -V mainfont="Montserrat" -V monofont="JetBrainsMono-Bold"
 
-if (!(Test-Path ".\$file_base_name.pdf")) {
-    Write-Host "Error: File could not be pandoced to .\$file_base_name.pdf."
+if (!(Test-Path "$file_base_name.pdf")) {
+    Write-Host "Error: File could not be pandoced to $file_base_name.pdf."
     Exit 1
 }
 
-Start-Process ".\$file_base_name.pdf"
+Start-Process "$file_base_name.pdf"
 
-if (Test-Path ".\plantuml-images") {
-    Remove-Item ".\plantuml-images\*" -Force
+if (Test-Path "plantuml-images") {
+    Remove-Item "plantuml-images\*" -Force
 }
 
